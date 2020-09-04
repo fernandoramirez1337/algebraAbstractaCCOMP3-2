ed89ee92806fd195254d2d93c7f5d9ede48a900b
Fernando Ramirez <31838677+fernandoramirez1337@users.noreply.github.com>
2020-09-04 12:23:36 -0500

    Código de la división entera (Tarea 2)

---
+++ divisionEntera.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int divisionEntera(int a, int n)
+{
+	int q = a / n;
+	int r = a - (q * n);
+	if (a<0)
+	{
+		q--;
+		//r += n;
+	}
+	//cout << r << endl;
+	return q;
+}
+
+int main()
+{
+	
+	cout << divisionEntera(255, 11) << endl;
+	cout << divisionEntera(-255, 11) << endl;
+    
+	return 0;
+
+}
